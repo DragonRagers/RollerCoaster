c4442d3127c996b4d7292fe0c4a16fda6667beed
Nguyen <374512@757-316-m10.local>
2018-12-13 08:32:51 -0500

    Made some files

---
+++ RollerCoaster/src/roller/coaster/Point.java
@@ -0,0 +1,6 @@
+package roller.coaster;
+
+public class Point {
+	//Used for position
+	public float x, y, z;
+}
---
+++ RollerCoaster/src/roller/coaster/Track.java
@@ -0,0 +1,7 @@
+package roller.coaster;
+
+public abstract class Track {
+	//All track pieces should be subclasses of this
+	Point end; //end point of the track piece relative to beginning (aka assume beginning is (0,0,0))
+	
+}
