f807de6c7ecdcbcae1ae774057806560b59fbf10
Luthra <370333@757-316-m12.local>
2018-12-17 09:13:30 -0500

    Scanner for weight

--- RollerCoaster/src/roller/coaster/main.java
+++ RollerCoaster/src/roller/coaster/main.java
@@ -1,5 +1,12 @@
 package roller.coaster;
 
+import java.util.*;
+
 public class main {
 
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Weight of the Cart: ");
+		int weight = keyboard.nextInt();
+	}
 }
