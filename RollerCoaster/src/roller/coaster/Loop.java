366da478f3b0c3a5bc95a6612afe8640d2b3a023
Naval <338794@757-316-m11.local>
2018-12-17 09:02:43 -0500

    fixed some things

--- RollerCoaster/src/roller/coaster/Loop.java
+++ RollerCoaster/src/roller/coaster/Loop.java
@@ -2,8 +2,6 @@ package roller.coaster;
 
 public class Loop extends Track {
 	public float radius;
-	public float init;
-	public Point end;
 	
 	public Loop(float r) {
 		radius = r;
@@ -15,4 +13,13 @@ public class Loop extends Track {
 		//f(t)=		{<(t-5)/(2pi), r + rcos(t-5), rsin(t-5)>		5<t<5+2pi
 		//			{<1, r + r*t/5, 0>							5+2pi<t<10+2pi	
 	}
+
+	@Override
+	public void drawTrack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
--- RollerCoaster/src/roller/coaster/Track.java
+++ RollerCoaster/src/roller/coaster/Track.java
@@ -3,5 +3,16 @@ package roller.coaster;
 public abstract class Track {
 	//All track pieces should be subclasses of this
 	Point end; //end point of the track piece relative to beginning (aka assume beginning is (0,0,0))
+	Point init;
+	public void setInit(Point p) {
+		init = p;
+	}
+	
+	public Point getEnd(Point p) {
+		return end;
+	}
+	
+	public abstract void drawTrack();
+	
 	
 }
