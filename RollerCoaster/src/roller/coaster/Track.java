c1284a15b5b3178eccb599f9ec4461a8a32d352c
Nguyen <374512@757-316-m10.local>
2018-12-19 09:07:47 -0500

    Working on implementing arclength calculations

--- RollerCoaster/src/roller/coaster/Track.java
+++ RollerCoaster/src/roller/coaster/Track.java
@@ -4,6 +4,8 @@ public abstract class Track {
 	//All track pieces should be subclasses of this
 	Point end; //end point of the track piece relative to beginning (aka assume beginning is (0,0,0))
 	Point init;
+	Double arcLength;
+	
 	public void setInit(Point p) {
 		init = p;
 	}
--- RollerCoaster/src/roller/coaster/VerticalLoop.java
+++ RollerCoaster/src/roller/coaster/VerticalLoop.java
@@ -3,8 +3,10 @@ package roller.coaster;
 public class VerticalLoop extends Track {
 	public float radius;
 	
-	public VerticalLoop(float r) {
+	public VerticalLoop(Point i, float r, int dir) { //dir is direction, 0 - x, 1 - y (unimplemented assuming y)
+		init = i;
 		radius = r;
+		end.y = init.y+2*radius; 
 		formTrack();
 	}
 	
@@ -13,9 +15,22 @@ public class VerticalLoop extends Track {
 		//f(t)=		{<(t-5)/(2pi), r + rcos(t-5), rsin(t-5)>		5<t<5+2pi
 		//			{<1, r + r*t/5, 0>							5+2pi<t<10+2pi	
 	}
+	
+	private Point calcPosition(double arcLength) {
+		if (arcLength < radius) {
+			return Point.add(init, new Point(0,arcLength,0));
+		} else if (arcLength < radius + radius * 2 * Math.PI) {
+			return Point.add(init, new Point(0,radius + radius * Math.cos(reParam(arcLength)), radius * Math.sin(reParam(arcLength))));
+		}
+		return null;
+	}
 
 	private double reParam(double arclength) { 
 		return arclength/Math.sqrt((1/Math.pow(Math.PI,2))+Math.pow(radius,2)); 
+	}
+	
+	private void calcLength() {
+		
 	}
 	
 	@Override
@@ -24,6 +39,10 @@ public class VerticalLoop extends Track {
 		
 	}
 
-	
+	public static void main(String args[]) { //testing shit
+		for (double i = 0; i < 20; i+=.1) {
+			System.out.println(calcPosition(i));
+		}
+	}
 	
 }
