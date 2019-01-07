d4196511a84d62c209dfccc01fcc3586cceaf0c2
Nguyen <374512@757-316-m10.local>
2019-01-07 07:56:47 -0500

    Started making bump track piece

---
+++ RollerCoaster/src/roller/coaster/Bump.java
@@ -0,0 +1,24 @@
+package roller.coaster;
+
+public class Bump extends TrackPiece {
+
+	public Bump () {
+		
+	}
+	
+	public void drawTrack() {
+		
+	}
+
+	public double arcLength() {
+		return 0;
+	}
+
+	public Point calcPosition(double arcLength) {
+		//a^(-(x)^b)
+		//larger a = wider
+		//lager b = more square
+		return null;
+	}
+
+}
